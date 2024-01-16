5b44fe813cad74dc2d369cb54d213d96d3d02856
Dhiraj Garad <yogeshsamse682@gmail.com>
2024-01-16 11:33:12 +0530

    added

--- movix-main/src/components/circleRating/CircleRating.jsx
+++ movix-main/src/components/circleRating/CircleRating.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+
 import "react-circular-progressbar/dist/styles.css";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 
 import "./style.scss";
 
